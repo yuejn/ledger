a0615e21f0b648c1c85d33d3eed823aa88af6192
Jenny <jenny@Mac.home>
2026-06-08 21:41:49 +0200

    Updates

---
+++ _next/static/LntT67WH026hnS4cZm1Vq/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fartist\u002F[slug]","\u002Fartists\u002F[slug]","\u002Ffestivals\u002F[slug]","\u002Flanguages\u002F[language]","\u002Fvisit\u002F[slug]","\u002Fyear\u002F[year]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
